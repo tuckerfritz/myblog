18971febc5d33b07f3fffe96916b293249200eb2
Tucker Fritz <tuckerpfritz@gmail.com>
2023-11-26 16:09:26 -0600

    Improve typing for card component. Remove unnecessary any type from rss.xml.

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,12 +1,12 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 
-export async function GET(context: any) {
+export async function GET() {
   const posts = await getCollection("posts");
   return rss({
     title: "Tucker Fritz | Blog",
     description: "My journey learning Astro",
-    site: context.site,
+    site: "https://tuckerfritz.dev/",
     items: posts.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
