69b3f3ffc909e7d6853df1ee535e421bceab82c1
Tucker Fritz <tuckerpfritz@gmail.com>
2023-12-01 00:44:13 -0600

    Added svelte to project.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import mdx from "@astrojs/mdx";
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(), 
-    mdx(),
-  ],
-  markdown: { 
+  integrations: [tailwind(), mdx(), svelte()],
+  markdown: {
     shikiConfig: {
-      theme: "dark-plus",
+      theme: "dark-plus"
     }
   },
   site: "https://tuckerfritz.dev"
